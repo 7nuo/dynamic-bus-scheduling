56e571ae9670a344cc8d0e284bdc645253af348a
pinac0099 <pinac0099@gmail.com>
2016-04-01 13:01:02 +0200

    Adding functions to connections.py

--- route-generator/connections.py
+++ route-generator/connections.py
@@ -12,14 +12,39 @@ class Connection(object):
         self.edges_collection = self.db.Edges
         self.address_book_collection = self.db.AddressBook
 
+    # Functions of Nodes
     def insert_node(self, osm_id, tags, point):
-        self.nodes_collection.insert_one({'osm_id': osm_id, 'tags': tags, 'point': point})
+        self.nodes_collection.insert_one({'osm_id': osm_id, 'tags': tags,
+                                          'point': {'longitude': point.longitude, 'latitude': point.latitude}})
 
     def find_node(self, osm_id):
-        return self.nodes_collection.find({'osm_id': osm_id})
+        return self.nodes_collection.find_one({'osm_id': osm_id})
 
     def delete_node(self, osm_id):
         result = self.nodes_collection.delete_one({'osm_id': osm_id})
+        return result.deleted_count
+
+    # Functions of Points
+    def insert_point(self, osm_id, point):
+        self.points_collection.insert_one({'osm_id': osm_id,
+                                           'point': {'longitude': point.longitude, 'latitude': point.latitude}})
+
+    def find_point(self, osm_id):
+        return self.points_collection.find_one({'osm_id': osm_id})
+
+    def delete_point(self, osm_id):
+        result = self.points_collection.delete_one({'osm_id': osm_id})
+        return result.deleted_count
+
+    # Functions of Ways
+    def insert_way(self, osm_id, tags, references):
+        self.ways_collection.insert_one({'osm_id': osm_id, 'tags': tags, 'references': references})
+
+    def find_way(self, osm_id):
+        return self.ways_collection.find_one({'osm_id': osm_id})
+
+    def delete_way(self, osm_id):
+        result = self.ways_collection.delete_one({'osm_id': osm_id})
         return result.deleted_count
 
 
